a03d4f63d8acee24fb6a52fbba30bf0be53ee662
Ken Girard <ken.girard@gmail.com>
2018-04-15 18:48:34 -0500

    new post works,,, but i broke all-post. single just started on

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ChinaIsADog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+secret_key='OICU812-WadUthink?'
 # added the current time to use later on, might need an import
 # current_time = datetime.datetime.utcnow()
 
@@ -17,16 +18,72 @@ class Blog(db.Model):
     # created = Column(DateTime, default=datetime.datetime.utcnow())
     
 
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
-        '''self.body = body'''
+        self.body = body
 
 
-@app.route('/blog', methods=['POST'])
+@app.route('/blog', methods=['POST','GET'])
 def blog_total():
-    all_post=Blog.query.order_by(Blog.id)
-    return render_template('blog.html', title="Build-A-Blog 101",
-    all_post=all_post)
+    all_posts=Blog.query.order_by(Blog.id).all()
+    print(all_posts)
+    return render_template('allpost.html',all_posts=all_posts)
+
+@app.route('/newpost')
+def display_newpost_form():
+    title=''
+    body=''
+    title_error=''
+    body_error=''
+    return render_template('newpost.html',title=title,body=body,title_error=title_error,
+                body_error=body_error)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+    '''title = request.form('title')
+    body = request.form('body')
+    title_error=''
+    body_error=''
+    '''
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+    
+    return redirect('/blog')
+
+    @app.route('/blog?id=')
+    def single_post():
+        singlepost = Blog.get_by_id(int(id))
+        return render_template('singlepost.html',singlepost=singlepost,title=title,body=body)
+
+
+    '''
+        if not title:
+            title_error = 'Please give it a title'
+        if not body:
+            body_error = 'Please write something'
+    
+        if title_error or body_error:
+            return render_template('newpost.html',title=title,body=body,title_error=title_error,
+                body_error=body_error)
+        else:
+            title = request.form('title')
+            body = request.form('body')
+            new_post = Blog(title, body)
+            db.session.add(new_post)
+            db.session.commit()
+        all_posts = Blog.query.order_by(id).all()
+        return render_template('allpost.html')
+        
+
+    
+    return render_template('newpost.html')
+    '''
+
 '''
 @app.route('/', methods=['POST', 'GET'])
 def index():
