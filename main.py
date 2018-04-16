d88e750274cd12bb4f41e6433f809018b21d0a8c
Ken Girard <ken.girard@gmail.com>
2018-04-15 22:33:25 -0500

    Validation added, and fixed the part that adds it to the db I broke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ChinaIsADog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-secret_key='OICU812-WadUthink?'
+app.secret_key='OICU812-WadUthink?'
 # added the current time to use later on, might need an import
 # current_time = datetime.datetime.utcnow()
 
@@ -33,34 +33,32 @@ def blog_total():
 def display_newpost_form():
     title=''
     body=''
-    title_error=''
-    body_error=''
-    
-    return render_template('newpost.html',title=title,body=body,title_error=title_error,
-                body_error=body_error)
+    return render_template('newpost.html',title=title,body=body)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
-    '''title = request.form('title')
-    body = request.form('body')
-    title_error=''
-    body_error=''
-    '''
-
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        new_post = Blog(title, body)
-        db.session.add(new_post)
-        db.session.commit()
-        # flash('New post added')
-    
-    return redirect('/blog')
+        if title and body:
+            title = title
+            body = body
+            new_post = Blog(title, body)
+            db.session.add(new_post)
+            db.session.commit()
+            flash("New post added")
+            # all_posts = Blog.query.order_by(id).all()
+            return redirect('/blog')
+        else:
+            flash("We need a title and text in the body of the post")
+            return render_template('newpost.html',title=title,body=body)
+
 
 @app.route('/singlepost', methods=['GET','POST'])
 def single_post():
     # TODO: Look at delete_task in get-it-done
     post_ided = Blog.query.get(id)
+    print(post_ided)
     singlepost="?id="+ post_ided
     print(singlepost)
     return redirect('/blog{singlepost}' .format(singlepost=singlepost))
